ed8d8a24b29a9527d648355a272789d796cfbd15
Pankaj Dagar <pankaj.dagar@spinny.com>
2023-01-30 23:40:32 +0530

    not found page handling

---
+++ src/components/404Page/index.jsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+
+const NotFoundPage = () => {
+  return (
+    <Typography variant="h1" style={{ color: 'white' }}>
+      Not Found
+    </Typography>
+  );
+};
+export default NotFoundPage;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,20 +1,21 @@
 import { Box, CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { drawerWidth } from '../../globalConstants/appConstants';
+import NotFoundPage from '../404Page';
 import Discover from '../Discover';
 import Header from '../Header';
 import SideBar from '../SideBar';
 
 const Layout = () => {
-
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <Header/>
+      <Header />
       <SideBar />
       <Box component="main" sx={{ flexGrow: 1, p: 3, overflowX: 'auto', width: { sm: `calc(100% - ${drawerWidth}px)` }, bgcolor: '#273244', height: '100vh' }}>
         <Routes>
           <Route path="/" element={<Discover />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Box>
     </Box>
